f083603182b0080eee64915d048419176e37eb6d
João Pedro Volponi <jpvrosada@hotmail.com>
2025-01-25 19:49:45 -0300

    Projects data Mobile

--- src/data/projects-data-mobile.ts
+++ src/data/projects-data-mobile.ts
@@ -11,61 +11,61 @@ const ProjectsDataMobile = [
     id: 1,
     img: swiftpow,
     title: "Swift POW",
-    description: "Organização sem fins lucrativos com o objetivo de ensinar conceitos, técnicas de desenvolvimento com Swift ",
+    description:
+      "Organização sem fins lucrativos com o objetivo de ensinar conceitos, técnicas de desenvolvimento com Swift ",
     tool: "Swift - Mobile",
     github: "https://github.com/orgs/Swift-P-O-W/repositories",
     link: "https://www.instagram.com/swift.pow/",
-    platform: "mobile",
   },
   {
     id: 2,
     img: salvae,
     title: "salvAÊ",
-    description: "Um Tour Gastronômico por Maringá que te oferece um produto em dobro em diversos estabelecimentos selecionados, o que te garante uma economia de mais de R$4.000,00.",
+    description:
+      "Um Tour Gastronômico por Maringá que te oferece um produto em dobro em diversos estabelecimentos selecionados, o que te garante uma economia de mais de R$4.000,00.",
     tool: "React Native",
     github: " ",
     link: "https://www.salvae.com.br/",
-    platform: "mobile",
   },
   {
     id: 3,
     img: tcc,
     title: "TCC Moedas",
-    description: "Projeto de conclusão de curso, onde foi desenvolvido uma aplicação que consome dados da API HG Brasil, para mostrar valores de moedas em tempo real e simulação de compra e venda.",
+    description:
+      "Projeto de conclusão de curso, onde foi desenvolvido uma aplicação que consome dados da API HG Brasil, para mostrar valores de moedas em tempo real e simulação de compra e venda.",
     tool: "Swift",
     github: "https://github.com/JoaoPedroVolponi/TCC",
     link: " ",
-    platform: "mobile",
   },
   {
     id: 4,
     img: viacep,
     title: "Via CEP",
-    description: "Aplicação que consome a API do Via CEP, para mostrar informações de endereços com base no CEP inserido.",
+    description:
+      "Aplicação que consome a API do Via CEP, para mostrar informações de endereços com base no CEP inserido.",
     tool: "Swift",
     github: "https://github.com/JoaoPedroVolponi/Via-CEP",
     link: " ",
-    platform: "mobile",
   },
   {
     id: 5,
     img: yaris,
     title: "Álcool ou Gasolina",
-    description: "Aplicação que calcula qual é a melhor opção de abastecimento para o seu veículo, com base no preço do álcool e da gasolina.",
+    description:
+      "Aplicação que calcula qual é a melhor opção de abastecimento para o seu veículo, com base no preço do álcool e da gasolina.",
     tool: "Swift",
     github: "https://github.com/JoaoPedroVolponi/AlcoolOuGasolina-Yaris",
     link: " ",
-    platform: "mobile",
   },
   {
     id: 6,
     img: brasileirao,
     title: "Brasileirão",
-    description: "Aplicação que consome a API Futebol e demonstra a tabela de classificação do Campeonato Brasileiro.",
+    description:
+      "Aplicação que consome a API Futebol e demonstra a tabela de classificação do Campeonato Brasileiro.",
     tool: "Swift",
     github: "https://github.com/JoaoPedroVolponi/Brasileirao",
     link: " ",
-    platform: "mobile",
   },
 ];
 
