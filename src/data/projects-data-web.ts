49a91afdf50aa48ef97f7f0ac5e5fa5b45d2557b
João Pedro Volponi <jpvrosada@hotmail.com>
2025-01-31 16:53:22 -0300

    Projetos Web

--- src/data/projects-data-web.ts
+++ src/data/projects-data-web.ts
@@ -1,9 +1,19 @@
-import swiftpow from "../assets/projects/org/swiftpow.png";
+import JoaoPedroVolponi from "../assets/projects/web/joao-pedro-volponi.png";
+import SalvaeWeb from "../assets/projects/web/salvae-web.png";
 
 const ProjectsDataWeb = [
   {
     id: 1,
-    img: swiftpow,
+    img: JoaoPedroVolponi,
+    title: "salvAÊ",
+    description: "Página de demonstração do salvAÊ e área administrativa.",
+    tool: "Angular",
+    github: " ",
+    link: "https://www.salvae.com.br/",
+  },
+  {
+    id: 2,
+    img: SalvaeWeb,
     title: "salvAÊ",
     description: "Página de demonstração do salvAÊ e área administrativa.",
     tool: "Angular",
