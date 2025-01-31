f8fadb008f227008f54d0b279fb24b804b9b328e
João Pedro Volponi <jpvrosada@hotmail.com>
2025-01-31 16:56:37 -0300

    Padronização de nomenclatura

--- src/data/projects-data-web.ts
+++ src/data/projects-data-web.ts
@@ -1,10 +1,10 @@
-import JoaoPedroVolponi from "../assets/projects/web/joao-pedro-volponi.png";
-import SalvaeWeb from "../assets/projects/web/salvae-web.png";
+import joaopedrovolponi from "../assets/projects/web/joao-pedro-volponi.png";
+import salvaeweb from "../assets/projects/web/salvae-web.png";
 
 const ProjectsDataWeb = [
   {
     id: 1,
-    img: SalvaeWeb,
+    img: salvaeweb,
     title: "salvAÊ",
     description: "Página de demonstração do salvAÊ e área administrativa.",
     tool: "Angular",
@@ -13,7 +13,7 @@ const ProjectsDataWeb = [
   },
   {
     id: 2,
-    img: JoaoPedroVolponi,
+    img: joaopedrovolponi,
     title: "Portfólio João Pedro Volponi",
     description: "Portfólio de Projetos",
     tool: "React",
